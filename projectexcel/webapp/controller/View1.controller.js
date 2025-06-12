dd0312651843b0029e746ccad8907cc736832578
Kapil Bidua <kapilvidua001@gmail.com>
2025-06-12 16:01:34 +0000

    Your commit message here

--- projectexcel/webapp/controller/View1.controller.js
+++ projectexcel/webapp/controller/View1.controller.js
@@ -1,10 +1,27 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+      "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("projectexcel.controller.View1", {
-        onInit() {
-        }
+        onInit: function () {
+            this.getView().setModel(new JSONModel([]), "excel");
+          },
+       
+          onUpload: function (oEvent) {
+            var file = oEvent.getParameter("files")[0];
+            if (file && window.FileReader) {
+              var reader = new FileReader();
+              reader.onload = function (e) {
+      var data = e.target.result;
+      var workbook = XLSX.read(data, { type: "binary" });
+                var sheetName = workbook.SheetNames[0];
+                var jsonData = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+                this.getView().getModel("excel").setData(jsonData);
+              }.bind(this);
+              reader.readAsBinaryString(file);
+            }
+          }
     });
 });
